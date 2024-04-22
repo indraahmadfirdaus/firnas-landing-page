c1de221c55b47d930487b25535736fbd5e827643
Indra <indraahmadfirdaus@gmail.com>
2024-04-22 21:38:52 +0700

    coming soon

---
+++ app/src/app/page.js
@@ -0,0 +1,14 @@
+import ComingSoon from "@/components/ComingSoon";
+import Hero from "@/components/Hero";
+import { TypewriterEffectDemo } from "@/components/TypewriterEffect";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+  <TypewriterEffectDemo />
+   <ComingSoon />
+    </>
+
+  );
+}
---
+++ app/src/components/ComingSoon.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ComingSoon = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-[40rem] ">
+    <p className="text-neutral-600 dark:text-neutral-200 text-4xl  mb-10">
+      Coming Soon.
+    </p>
+
+  </div>
+  )
+}
+
+export default ComingSoon
